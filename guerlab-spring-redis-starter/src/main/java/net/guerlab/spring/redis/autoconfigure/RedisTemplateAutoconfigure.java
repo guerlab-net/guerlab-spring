117141ce37a42aa88475a45c3148302dad56af62
guerlab <master@guerlab.net>
2021-03-24 12:15:24 +0800

    update to 4.0.0-SNAPSHOT

--- guerlab-spring-redis-starter/src/main/java/net/guerlab/spring/redis/autoconfigure/RedisTemplateAutoconfigure.java
+++ guerlab-spring-redis-starter/src/main/java/net/guerlab/spring/redis/autoconfigure/RedisTemplateAutoconfigure.java
@@ -1,13 +1,11 @@
 package net.guerlab.spring.redis.autoconfigure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * RedisTemplate自动配置
@@ -29,18 +27,13 @@ public class RedisTemplateAutoconfigure {
      */
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Bean
-    @ConditionalOnBean({ RedisConnectionFactory.class, ObjectMapper.class })
-    public RedisTemplate<String, ?> redisTemplate(RedisConnectionFactory factory, ObjectMapper objectMapper) {
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory, ObjectMapper objectMapper) {
         ObjectMapper mapper = objectMapper.copy();
-
         mapper.activateDefaultTyping(mapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
 
-        RedisTemplate<String, ?> template = new RedisTemplate<>();
-
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-
+        template.setKeySerializer(new GenericJackson2JsonRedisSerializer(mapper));
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));
 
         return template;
